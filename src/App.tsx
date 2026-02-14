0d5ecadf3f8bfb94d5f8f5c7f59f63e389b80f6e
jsales2 <jsales0204@gmail.com>
2026-02-13 23:42:20 -0500

    functionality to remove saved urls

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,61 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 import { X } from "lucide-react";
 
 function App() {
-    const [inputtedUrl, setInputtedUrl] = useState<String>("");
-    const [storedUrls, setStoredUrls] = useState<String[]>([]);
+    const [inputtedUrl, setInputtedUrl] = useState<string>("");
+    const [storedUrls, setStoredUrls] = useState<string[]>([]);
+
+    //get urls to display when extension is opened
+    useEffect(() => {
+        getUrls();
+        async function getUrls() {
+            const result = await chrome.storage.sync.get<{ urls: string[] }>("urls");
+            const existingUrls = result.urls || [];
+            setStoredUrls(existingUrls);
+        }
+    }, [])
 
     async function addUrl() {
         try {
             //get currently stored urls
             const result = await chrome.storage.sync.get<{ urls: string[] }>("urls");
             const existingUrls = result.urls || [];
+            //ensures no duplicates are added
+            if (existingUrls.includes(inputtedUrl)) {
+                return;
+            }
             // append inputted url to it
             const updatedUrls = [...existingUrls, inputtedUrl];
             setStoredUrls(updatedUrls);
             await chrome.storage.sync.set({ urls: updatedUrls });
-
         } catch(error) {
             console.log(error);
         }
     }
 
-    async function removeUrl() {
+    async function removeUrl(urlToRemove: string) {
         try {
-
+            const updatedUrls = storedUrls.filter((url) => url != urlToRemove);
+            setStoredUrls(updatedUrls);
+            await chrome.storage.sync.set({ urls: updatedUrls });
         } catch(error) {
             console.log(error);
         }
     }
 
     return (
-        <div className="size-150">
-            <p className='text-gray-800'>focus</p>
+        <div className="h-150 w-100 justify-center p-5">
+            <p className='text-gray-800 text-lg text-center mb-1'>Focus :)</p>
             <div className="flex gap-2 max-w-lg">
                 <Input placeholder={"Enter URL to Block"} onChange={(e) => setInputtedUrl(e.target.value)}/>
                 <Button className="hover:cursor-pointer" onClick={addUrl}>Add URL</Button>
             </div>
             {storedUrls.map((url, index) => (
-                <div>
-                    <p key={index}>{url}</p>
-                    <Button variant={"destructive"}>
+                <div key={index} className="flex justify-center gap-2 mt-5">
+                    <p className="text-lg">{url}</p>
+                    <Button variant={"destructive"} className="hover:cursor-pointer size-8" onClick={() => removeUrl(url)}>
                         <X/>
                     </Button>
                 </div>
