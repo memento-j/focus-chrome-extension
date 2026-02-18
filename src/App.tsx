191db1542697d17c57bacd66b2f1dab354fcbe34
jsales2 <jsales0204@gmail.com>
2026-02-18 18:30:26 -0500

    added darker theme

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { X } from "lucide-react";
 
 function App() {
     const [inputtedUrl, setInputtedUrl] = useState<string>("");
-    const [storedUrls, setStoredUrls] = useState<string[]>([]);
+    const [storedUrls, setStoredUrls] = useState<string[]>(["meow", "woof", "bark"]);
 
     //get urls to display when extension is opened
     useEffect(() => {
@@ -29,6 +29,7 @@ function App() {
             // append inputted url to it
             const updatedUrls = [...existingUrls, inputtedUrl];
             setStoredUrls(updatedUrls);
+            setInputtedUrl("");
             await chrome.storage.sync.set({ urls: updatedUrls });
         } catch(error) {
             console.log(error);
@@ -46,15 +47,15 @@ function App() {
     }
 
     return (
-        <div className="h-150 w-100 justify-center p-5">
-            <p className='text-gray-800 text-lg text-center mb-1'>Focus :)</p>
+        <div className="h-150 w-100 justify-center p-5 bg-[#0F0F11]/85">
+            <p className='text-2xl text-center mb-3 text-zinc-400'>Focus :)</p>
             <div className="flex gap-2 max-w-lg">
-                <Input placeholder={"Enter URL to Block"} onChange={(e) => setInputtedUrl(e.target.value)}/>
+                <Input className="text-zinc-400 border-[#0F0F11]/50" placeholder={"Enter URL to Block"} value={inputtedUrl} onChange={(e) => setInputtedUrl(e.target.value)}/>
                 <Button className="hover:cursor-pointer" onClick={addUrl}>Add URL</Button>
             </div>
             {storedUrls.map((url, index) => (
                 <div key={index} className="flex justify-center gap-2 mt-5">
-                    <p className="text-lg">{url}</p>
+                    <p className="text-lg text-zinc-400">{url}</p>
                     <Button variant={"destructive"} className="hover:cursor-pointer size-8" onClick={() => removeUrl(url)}>
                         <X/>
                     </Button>
